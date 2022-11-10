43da9d15c9f3abad899bd486d47b60bd128b0c5e
JsArafath <arafatrahman077777@gmail.com>
2022-11-10 23:04:20 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Partsix Server side
---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const app = express();
+const port = process.env.PORT || 5002;
+
+app.get('/', (req, res) => {
+    res.send('Simple Node Server Running Yo');
+});
+
+app.use(cors());
+app.use(express.json());
+
+const users = [
+
+];
+
+// username: dbUser1
+// password: eMAUs96DIUBHS9zN
+
+
+
+const uri = "mongodb+srv://dbUser1:eMAUs96DIUBHS9zN@cluster0.w0dynz3.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+async function run() {
+    try{
+        const serviceCollection = client.db('tour').collection('services'); 
+        const reviewCollection = client.db('tour').collection('review'); 
+
+        // app.get( '/services', async (req, res) =>{
+        //     const cursor = serviceCollection.find({});
+        //     const services = await cursor.toArray();
+        //     res.send(services);
+        // })
+
+       
+// POST API 
+
+// service post api 
+app.post('/services' , async (req, res) => {
+  const data =  req.body;
+  const result = await serviceCollection.insertOne(data);
+  console.log(result);
+  res.send(result);
+})
+
+// review post api 
+app.post('/review' , async (req, res) => {
+  const data =  req.body;
+  const result = await reviewCollection.insertOne(data);
+  console.log(result);
+  res.send(result);
+})
+
+
+// GET API 
+// service get api
+app.get('/services', async (req, res) => {
+  const cursor = serviceCollection.find({});
+  const data = await cursor.toArray();
+  res.json(data);
+})
+
+// service details 
+app.get('/service/:id', async(req, res) => {
+   const id = req.params.id;
+   const query = {_id: ObjectId(id)};
+   const booking = await serviceCollection.findOne(query);
+   res.send(booking);
+})
+
+// review get api 
+app.get('/review', async (req, res) => {
+  const cursor = reviewCollection.find({});
+  const data = await cursor.toArray();
+  res.json(data);
+})
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err =>console.log(err))
+
+
+
+
+app.listen(port, () => {
+    console.log(`Simple not server running on port ${port}`);
+})
+
+
+
