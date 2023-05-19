b73a96ebcdb072b41ad3306ad4795ae6f750febe
JsArafath <arafath.91289@gmail.com>
2023-05-20 00:45:30 +0600

    something  update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const users = [
 
 
 
-const uri = "mongodb+srv://dbUser1:eMAUs96DIUBHS9zN@cluster0.w0dynz3.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w0dynz3.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
