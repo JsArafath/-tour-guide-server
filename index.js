0b3b19ae23f4643fc03d7e2ecd8752c366f1f1b4
JsArafath <arafath.91289@gmail.com>
2023-05-20 00:51:25 +0600

    up

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const users = [
 const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w0dynz3.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// 
 
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
